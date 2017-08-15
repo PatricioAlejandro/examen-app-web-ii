1f8db505242ded4a4a0695bab281abad994783c8
Patricio Chavez <patricio.chavez@epn.edu.ec>
2017-08-14 19:19:13 -0500

    crear apis usuario,pizza,ingrediente

---
+++ compraPizza/api/controllers/PizzaController.js
@@ -0,0 +1,11 @@
+/**
+ * PizzaController
+ *
+ * @description :: Server-side logic for managing Pizzas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
